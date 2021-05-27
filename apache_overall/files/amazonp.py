2fa16240c21675a3608fa31ec63c728ded17d7d1
sidd <sidd@gmail.com>
2021-05-27 05:24:17 +0000

    version 7

--- apache_overall/files/amazonp.py
+++ apache_overall/files/amazonp.py
@@ -3,11 +3,11 @@ import subprocess
 a1 = subprocess.check_output("ps -ef | grep -i httpd | grep -v grep | wc -l", shell=True)
 a2 = subprocess.check_output("ps -ef | grep -i sshd | grep -v grep | wc -l", shell=True)
 
-if (int(a1)==3 and int(a2)==3):
+if (int(a1)==6 and int(a2)==4):
     print("All processes are up and running")
-elif (int(a1)==3 and int(a2)!=3):
+elif (int(a1)==6 and int(a2)!=4):
     print("SSHD process is not running")
-elif (int(a1)!=3 and int(a2)==3):
+elif (int(a1)!=6 and int(a2)==4):
     print("HTTPD process is not running")
-elif (int(a1)!=3 and int(a2)!=3):
+elif (int(a1)!=6 and int(a2)!=4):
     print("HTTPD and SSHD processes are not running") 
